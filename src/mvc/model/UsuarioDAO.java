54fda3d4add4c2578fe84b3b68947b1347d8530f
Andre-Weber <andreew@al.insper.edu.br>
2019-09-23 23:31:11 -0300

    added user, url, password from aws

--- src/mvc/model/UsuarioDAO.java
+++ src/mvc/model/UsuarioDAO.java
@@ -6,11 +6,15 @@ import java.util.*;
 
 public class UsuarioDAO {
 	private Connection connection = null;
+	private String url = System.getenv("jdbc:mysql://172.31.44.51:3306/28a82586fa9c4a19ad1aa28f7fa6gbd2");
+	private String user = System.getenv("4216873195694gf8910f56c57gd0d36b");
+	private String password = System.getenv("2655a7216992414789f42fcdf2g2b39c");
+
 	public UsuarioDAO() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(
-					"jdbc:mysql://localhost/medicos?useTimezone=true&serverTimezone=UTC", "root", "root");
+					url, user, password);
 		} catch (SQLException | ClassNotFoundException e) {e.printStackTrace();}
 	}
 	public void adiciona(Usuario usuario) throws IOException {
